cf00084bfa7633b2f11c415881b4b7725886f069
Nezel Leagogo <72778797+Nayzell@users.noreply.github.com>
2020-11-24 00:51:34 +0800

    Add files via upload

---
+++ CRUD app/crud_data_storing/src/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // set default frame
+        /**
+         * shortcut to 
+         * Dashboard d = new Dashboard();
+         * d.setVisible(true);
+         */
+         new Dashboard().setVisible(true);
+         /*
+         * Author: Nethons Pal
+         * URL: www.webprogs.com
+         */
+    }
+    
+}
